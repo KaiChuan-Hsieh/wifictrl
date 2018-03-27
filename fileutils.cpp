14bf80ad95bed5275d26e362173dffc367c9a8f0
Kai-Chuan Hsieh <kai.chiuan@gmail.com>
2018-03-27 14:38:14 +0800

    Update start hostapd code management

---
+++ fileutils.cpp
@@ -0,0 +1,85 @@
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+char *get_config_value(const char *file_path, const char *key)
+{
+    FILE *fp;
+    char line[256];
+    char buf[256];
+    char val[256];
+    char *cptr;
+    char *lptr;
+
+    printf("%s In\n", __func__);
+
+    fp = fopen(file_path, "r");
+
+    if (!fp)
+        return NULL;
+
+    while (fgets(line, 256, fp)) {
+        memset(buf, 0, 256);
+        memset(val, 0, 256);
+
+        for (lptr = line; *lptr == ' '; lptr++)
+            ;
+        if (*lptr == '#' || *lptr == '\n' || *lptr == '\0')
+            continue;
+
+        cptr = strchr(lptr, '=');
+
+        if (cptr) {
+            strncpy(buf, lptr, cptr - lptr);
+            strncpy(val, cptr + 1, strlen(lptr) - (cptr - lptr + 1));
+
+            cptr = strpbrk(val, "\n# ");
+            if (cptr)
+                *cptr = 0;
+
+            if (strcmp(buf, key) == 0) {
+                break;
+            }
+        }
+    }
+
+    fclose(fp);
+
+    cptr = strdup(val);
+
+    printf("%s Out\n", __func__);
+
+    return cptr;
+}
+
+pid_t get_pid(const char *file_path)
+{
+    FILE *fp;
+    char line[256];
+    int i = 0;
+    pid_t pid;
+
+    fp = fopen(file_path, "r");
+    if (!fp)
+        return -1;
+
+    while (fgets(line, 256, fp)) {
+        for (i = 0; i < sizeof(line); i++) {
+            if (!isdigit(line[i])) {
+                line[i] = '\0';
+                break;
+            }
+        }
+    }
+
+    fclose(fp);
+
+    if (i > 0)
+        pid = atoi(line);
+    else
+        pid = -1;
+
+    return pid;
+}
--- hostapd_ctrl.cpp
+++ hostapd_ctrl.cpp
@@ -4,98 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <sys/ioctl.h>
-
-#include <net/if.h>
 
 #include "hostapd_config.h"
 
 #define DEFAULT_PID_PATH "/var/run/hostapd/hostapd.pid"
 
-char *get_interface(char *file_path)
-{
-    FILE *fp;
-    char line[256];
-    char key[256];
-    char val[256];
-    char *cptr;
-    char *lptr;
-    char *ifname = NULL;
-
-    printf("%s In\n", __func__);
-
-    fp = fopen(file_path, "r");
-
-    if (!fp)
-        return NULL;
-
-    while (fgets(line, 256, fp)) {
-        memset(key, 0, 256);
-        memset(val, 0, 256);
-
-        for (lptr = line; *lptr == ' '; lptr++)
-            ;
-        if (*lptr == '#' || *lptr == '\n' || *lptr == '\0')
-            continue;
-
-        cptr = strchr(lptr, '=');
-
-        if (cptr) {
-            strncpy(key, lptr, cptr - lptr);
-            strncpy(val, cptr + 1, strlen(lptr) - (cptr - lptr + 1));
-
-            cptr = strpbrk(val, "\n# ");
-            if (cptr)
-                *cptr = 0;
-
-            if (strcmp(key, "interface") == 0) {
-                ifname = strdup(val);
-                break;
-            } else
-                continue;
-        }
-    }
-
-    fclose(fp);
-
-    printf("%s Out\n", __func__);
-
-    return ifname;
-}    
-
-pid_t get_pid(const char *file_path)
-{
-    FILE *fp;
-    char line[256];
-    int i = 0;
-    pid_t pid;
-
-    fp = fopen(file_path, "r");
-    if (!fp)
-        return -1;
-
-    while (fgets(line, 256, fp)) {
-        for (i = 0; i < sizeof(line); i++) {
-            if (!isdigit(line[i])) {
-                line[i] = '\0';
-                break;
-            }
-        }
-    }
-
-    fclose(fp);
-
-    if (i > 0)
-        pid = atoi(line);
-    else
-        pid = -1;
-
-    return pid;
-}
+extern char *get_config_value(const char *file_path, const char *key);
+extern int get_interface_state(const char *ifname);
+extern pid_t get_pid(const char *file_path);
 
 void free_params(char *argv[])
 {
@@ -106,29 +25,7 @@ void free_params(char *argv[])
     }
 }
 
-int get_interface_state(char *ifname)
-{
-    int ifc_sock;
-    struct ifreq ifr;
-
-    ifc_sock = socket(AF_INET, SOCK_DGRAM | SOCK_CLOEXEC, 0);
-    if (ifc_sock < 0) {
-        printf("socket() failed: %s\n", strerror(errno));
-        return -1;
-    }
-
-    memset(&ifr, 0, sizeof(struct ifreq));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
-    ifr.ifr_name[IFNAMSIZ - 1] = 0;
-
-    if(ioctl(ifc_sock, SIOCGIFFLAGS, &ifr) < 0)
-        return -1;
-
-    close(ifc_sock);
-    return ifr.ifr_flags & IFF_UP;
-}
-
-int hostapd_ctrl_start(char *hostapd_path, char *config_path)
+int hostapd_ctrl_start(const char *hostapd_path, const char *config_path)
 {
     pid_t pid;
     char *pid_path = strdup(DEFAULT_PID_PATH);
@@ -144,26 +41,26 @@ int hostapd_ctrl_start(char *hostapd_path, char *config_path)
         kill(pid, SIGTERM);
     }
 
-    if (ifname = get_interface(config_path)) {
+    if (ifname = get_config_value(config_path, "interface")) {
         while (1) {
             if (count > 5) {
-                free(ifname);
-                return -1;
+                ret = -1;
+                goto out;
             }
             if (ret < 0) {
-                free(ifname);
-                return -1;
+                goto out;
             }
             ret = get_interface_state(ifname);
             if (ret == 0) {
-                free(ifname);
                 break;
             }
             count++;
             sleep(1);
         }
-    } else
-        return -1;
+    } else {
+        ret = -1;
+        goto out;
+    }
 
     argv[0] = strdup(hostapd_path);
     argv[1] = strdup("-P");
@@ -189,7 +86,11 @@ int hostapd_ctrl_start(char *hostapd_path, char *config_path)
             ret = 0;
     }
 
-    free(pid_path);
+out:
+    if (pid_path)
+        free(pid_path);
+    if (ifname)
+        free(ifname);
     free_params(argv);
     printf("%s Out\n", __func__);
     return ret;
